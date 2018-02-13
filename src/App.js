c40fd59d52a10f45fbc36b140a7f263cc76b08f3
Ivan Trujillo <krival1987@gmail.com>
2018-02-13 18:50:40 -0500

    added Emmet support for jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import  Header  from './components/Header/header_component'
+import About from './components/About/about_component'
 
 class App extends Component {
   render() {
     return (
+     <div>
      <Header />
+     <main>
+      <About />
+     </main>
+    </div>
     );
   }
 }
