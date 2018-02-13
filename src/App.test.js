d31d4b6a7f1326781623f1878771ce904e1e8e0a
Ivan Trujillo <krival1987@gmail.com>
2018-02-13 16:28:44 -0500

    completed css grid layout of project

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,38 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+/*
+ Basic responsive design principles
+
+1. For responsive design it is good to use fluid grids and layout
+we use percentages instead of pixels for all layout lenghts specially width. This is for the content to adapt to the viewport the user is using.
+
+1. Flexible/responsive images
+Images behind different then text because they do not adapt to the viewport, we make them flexible by definiging their dimensions in percentages, otherwise it will not scale with fixed px.
+
+3. Media queries
+
+allow us to change styles for different devices by using media queries. amazing for different types of devices
+
+
+------- Layout Types in modern web design --------
+There are three ways to layout a webpage
+
+Float Layouts (modern standard)
+Current standard we will use float layouts
+
+FlexBox (new)
+Laysout elements in one direction row
+
+CSSGrid (newest)
+creates a two dimensional grid for design elements
+
+
+------ Build a Fluid Grid -------
+A grid is a design system that allows us to build consistent interfaces
+
+we can merge columns where we can make different configurations
+the columns are in a container called a row we must use rows and columns
+
+
+*/
--- src/components/Header/header_component.js
+++ src/components/Header/header_component.js
@@ -26,8 +26,37 @@ const Header = () =>{
     </h1>
      <a href="#here" className="btn btn--white btn--animated">Discover our tours</a>
     </div>
-
    </header>
+   // <section className="grid-test">
+   //  <div className="row">
+   //   <div className="col-1-of-2"> Col 1 of 2</div>
+   //   <div className="col-1-of-2">Col 1 of 2 </div>
+   //  </div>
+   //  <div className="row">
+   //   <div className="col-1-of-3"> Col 1 of 3</div>
+   //   <div className="col-1-of-3">Col 1 of 3 </div>
+   //   <div className="col-1-of-3"> Col 1 of 3</div>
+   //  </div>
+   //  <div className="row">
+   //   <div className="col-1-of-3"> Col 1 of 3</div>
+   //   <div className="col-2-of-3">Col 2 of 3 </div>
+   //  </div>
+   //  <div className="row">
+   //   <div className="col-1-of-4"> Col 1 of 4</div>
+   //   <div className="col-1-of-4">Col 1 of 4 </div>
+   //   <div className="col-1-of-4"> Col 1 of 4</div>
+   //   <div className="col-1-of-4"> Col 1 of 4</div>
+   //  </div>
+   //  <div className="row">
+   //   <div className="col-1-of-4">Col 1 of 4</div>
+   //   <div className="col-1-of-4">Col 1 of 4</div>
+   //   <div className="col-2-of-4">Col 2 of 4</div>
+   //  </div>
+   // <div className="row">
+   //  <div className="col-1-of-4">Col 1 of 4</div>
+   //  <div className="col-3-of-4">Col 3 of 4</div>
+   // </div>
+   // </section>
  )
 }
 
